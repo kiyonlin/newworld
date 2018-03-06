bdf80d21b98adf86cab78c173d92346704e69ecd
kiyon <kiyonlin@163.com>
2018-03-06 15:00:46 +0800

    learn function and reflect

---
+++ cmd/syntax/function/func.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"reflect"
+	"runtime"
+)
+
+func eval(a, b int, op string) (int, error) {
+	switch op {
+	case "+":
+		return a + b, nil
+	case "-":
+		return a - b, nil
+	case "*":
+		return a * b, nil
+	case "/":
+		q, _ := div(a, b)
+		return q, nil
+	default:
+		return 0, fmt.Errorf("unsupported operation:" + op)
+	}
+}
+
+func div(a, b int) (int, int) {
+	return a / b, a % b
+}
+
+func apply(op func(int, int) int, a, b int) int {
+	p := reflect.ValueOf(op).Pointer()
+	opName := runtime.FuncForPC(p).Name()
+	fmt.Printf("calling function %s with args (%d %d)\n", opName, a, b)
+	return op(a, b)
+}
+
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
+}
+
+func main() {
+	fmt.Println(eval(3, 4, "+"))
+	fmt.Println(eval(5, 6, "/-"))
+	q, r := div(13, 5)
+	fmt.Println("div(13, 5):", q, r)
+
+	fmt.Println("Apply pow(3,4):", apply(pow, 3, 4))
+}
