1db6938adc2fe3fef0ed368a0199658ed38bec71
kiyon <kiyonlin@163.com>
2018-03-07 09:51:22 +0800

    learn variable params

--- cmd/syntax/function/func.go
+++ cmd/syntax/function/func.go
@@ -38,6 +38,14 @@ func pow(a, b int) int {
 	return int(math.Pow(float64(a), float64(b)))
 }
 
+func sum(numbers ...int) int {
+	s := 0
+	for _, value := range numbers {
+		s += value
+	}
+	return s
+}
+
 func main() {
 	fmt.Println(eval(3, 4, "+"))
 	fmt.Println(eval(5, 6, "/-"))
@@ -45,4 +53,11 @@ func main() {
 	fmt.Println("div(13, 5):", q, r)
 
 	fmt.Println("Apply pow(3,4):", apply(pow, 3, 4))
+
+	fmt.Println(
+		apply(func(a, b int) int {
+			return int(math.Pow(float64(a), float64(b)))
+		}, 3, 4))
+
+	fmt.Println(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
 }
