c5cc8df2e01530d82b1635f44bd63bf0ff41a418
kiyon <kiyonlin@163.com>
2018-03-07 15:04:01 +0800

    learn recover

---
+++ cmd/syntax/errorhandler/recover/recover.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
+func tryRecover() {
+	defer func() {
+		r := recover()
+		if err, ok := r.(error); ok {
+			log.Println("Error occured:", err)
+		} else {
+			panic(fmt.Sprintf("I don't know what to do:%v", r))
+		}
+	}()
+
+	panic(errors.New("this is an error"))
+	b := 0
+	a := 5 / b
+	fmt.Println(a)
+	panic(922)
+}
+
+func main() {
+	tryRecover()
+}
