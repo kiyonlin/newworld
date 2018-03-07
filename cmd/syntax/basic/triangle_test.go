ec45239aaffcd415a76fa65a7539c60fd70b4a53
kiyon <kiyonlin@163.com>
2018-03-07 14:14:13 +0800

    improve test

--- cmd/syntax/basic/triangle_test.go
+++ cmd/syntax/basic/triangle_test.go
@@ -7,11 +7,12 @@ func TestTriangle(t *testing.T) {
 		{3, 4, 5},
 		{5, 12, 13},
 		{8, 15, 17},
+		{30000, 40000, 50000},
 	}
 
 	for _, tt := range tests {
 		if actual := calcTriangle(tt.a, tt.b); actual != tt.c {
-			t.Error("calctrangle(%d,%d); got %d; expected:%d", tt.a, tt.b, tt.c)
+			t.Errorf("calctrangle(%d,%d); got %d; expected:%d", tt.a, tt.b, actual, tt.c)
 		}
 	}
 }
