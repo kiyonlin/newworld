03fd2622a8d5bd54085c32fd8968f52efd02be00
kiyon <kiyonlin@163.com>
2018-04-17 15:57:30 +0800

    add user profile type

---
+++ crawler/model/profile.go
@@ -0,0 +1,18 @@
+package model
+
+// Profile define a user profile
+type Profile struct {
+	Name          string
+	Gender        string
+	Age           int
+	Height        int
+	Weight        int
+	Income        string
+	Marriage      string
+	Education     string
+	Occupation    string
+	Hukou         string // 户口
+	Constellation string // 星座
+	House         string
+	Car           string
+}
