3158a38f5ee685a69699d84ec51c59aed192b686
kiyon <kiyonlin@163.com>
2018-04-17 15:02:20 +0800

    learn regexp

---
+++ cmd/syntax/regex/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+const text = "My email is jack@gmail.com " +
+	"My email is aa@gmail.com " +
+	"My email js jack@gmail.com.com"
+
+func main() {
+	re := regexp.MustCompile("([a-zA-Z0-9]+)@([a-zA-z0-9]+)\\.([a-zA-Z0-9.]+)")
+	amtch := re.FindAllStringSubmatch(text, -1)
+	fmt.Println(amtch)
+}
