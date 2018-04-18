0b8b65c0e5ad2f5a592e8ba5cf700991b974119a
kiyon <kiyonlin@163.com>
2018-04-18 13:44:38 +0800

    使用并发引擎抓取数据

--- cmd/crawler/main.go
+++ cmd/crawler/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"github.com/kiyonlin/newworld/crawler/engine"
+	"github.com/kiyonlin/newworld/crawler/scheduler"
 	"github.com/kiyonlin/newworld/crawler/zhenai/parser"
 )
 
 func main() {
 	//http://www.zhenai.com/zhenghun
-	engine.Run(engine.Request{URL: "http://www.zhenai.com/zhenghun", ParserFunc: parser.ParseCityList})
+	e := engine.ConcurrentEngine{
+		Scheduler:   &scheduler.SimpleScheduler{},
+		WorkerCount: 100,
+	}
+	e.Run(engine.Request{URL: "http://www.zhenai.com/zhenghun", ParserFunc: parser.ParseCityList})
 }
